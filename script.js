030b9589b8fd66beb757c43c5883c3ba7390ebab
Melvin Ezell <melvin@Melvins-MacBook-Pro.local>
2023-02-01 22:55:48 -0500

    BUILD: basic library

--- script.js
+++ script.js
@@ -1,47 +1,161 @@
-// "use strict";
-const bookshelf = document.querySelector(".bookshelf");
-const myLibrary = [];
+"use strict";
 
 function Library() {
-  this.myLibrary = [];
+  this.library = new Map();
 }
 
-function Book(author, title, pages) {
-  this.author = author;
-  this.title = title;
-  this.pages = pages;
-  this.completed = false;
-}
-
-Book.prototype.add = function () {
-  addBookToLibrary(this);
+const libraryMethods = {
+  addBook(id, book) {
+    this.library.set(id, { ...book, id });
+  },
+  deleteBook(id) {
+    this.library.delete(id);
+  },
+  updateBook(id, bool) {
+    this.library.get(id).completed = bool;
+  },
+  getBook(id) {
+    return this.library.get(id);
+  },
+  getBookArray() {
+    return Array.from(this.library.values());
+  },
+  getLibraryStats() {
+    return this.libraryStats;
+  },
+  getTotalBooks() {
+    return this.library.size;
+  },
+  getTotalOfCompletedBooks() {
+    return this.getBookArray().filter((book) => book.completed).length;
+  },
+  getTotalPagesRead() {
+    return this.getBookArray().reduce(
+      (acc, curr) => (acc += curr.completed ? parseInt(curr.pages) : 0),
+      0
+    );
+  },
+  getCompeletionScore() {
+    return this.getTotalBooks() === 0
+      ? 0
+      : Math.floor(
+          (this.getTotalOfCompletedBooks() / this.getTotalBooks()) * 100
+        );
+  },
 };
 
-function addBookToLibrary(newBook) {
-  myLibrary.push(newBook);
+Library.prototype = Object.create(libraryMethods);
+
+function ViewUI() {
+  this.template = document.getElementById("bookTemplate");
+  this.bookshelf = document.querySelector(".bookshelf");
+  this.totalBooks = document.querySelector("[data-book-total]");
+  this.completedBooks = document.querySelector("[data-completion-total]");
+  this.completionPercentage = document.querySelector(
+    "[data-completion-percentage]"
+  );
+  this.pagesRead = document.querySelector("[data-pages-read]");
 }
 
-function updateUI() {
-  myLibrary.forEach((item) => {
-    bookshelf.innerText += item.title;
+const uiMethods = {
+  updateBookshelfUI(libraryArray) {
+    this.bookshelf.innerText = "";
+    const bookNodes = libraryArray.map((book) => this.createCard(book));
+    this.bookshelf.append(...bookNodes);
+  },
+  createCard({ author, title, pages, id, completed } = {}) {
+    // FIX: pages and completed
+    const clone = this.template.content.cloneNode(true);
+    clone.querySelector(".book__title").innerText = title;
+    clone.querySelector(".book__author").innerText = `By: ${author}`;
+    clone.querySelector(".book__pages").innerText = `Pages: ${pages}`
+    clone.querySelector("[type=checkbox]").checked = completed;
+    clone.querySelector(".book").setAttribute("data-book-completed", completed);
+    clone.querySelector(".book").setAttribute("data-bookId", id);
+    return clone;
+  },
+  updateBookTotalUI(totalBooks) {
+    this.totalBooks.innerText = `Total Books: ${totalBooks}`;
+  },
+  updateCompletedBooksUI(number) {
+    this.completedBooks.innerText = `Books Completed: ${number}`;
+  },
+  updateCompletionPercentageUI(percentage) {
+    this.completionPercentage.innerText = `Completion percetage: ${percentage}%`;
+  },
+  updatePagesReadUI(pages) {
+    this.pagesRead.innerText = `Pages Read: ${pages}`;
+  },
+};
+
+ViewUI.prototype = Object.create(uiMethods);
+
+function Controller(model, view) {
+  this.model = model;
+  this.view = view;
+
+  this.bookshelf = document.querySelector(".bookshelf");
+  this.bookshelf.addEventListener("click", (e) => {
+    let bookCard = e.target.closest(".book");
+    if (bookCard) {
+      if (e.target.nodeName == "INPUT" || e.target.nodeName == "LABEL") {
+        // TODO
+        const bool = bookCard.dataset.bookCompleted === "true" ? false : true;
+        this.model.updateBook(bookCard.dataset.bookid, bool);
+      }
+      if (e.target.nodeName == "I" || e.target.nodeName == "BUTTON") {
+        this.model.deleteBook(bookCard.dataset.bookid);
+      }
+      this.handleUpdatingUI();
+    }
+  });
+
+  this.bookForm = document.querySelector(".book-form");
+  this.bookForm.addEventListener("submit", (e) => {
+    this.handleSubmission(e);
+    this.handleUpdatingUI();
   });
 }
-const dune = new Book("Frank Herbert", "Dune", 300);
-const harryPotter = new Book("Harry Potter", "Harry Potter", 230);
 
-dune.add();
-harryPotter.add();
+const controllerMethod = {
+  handleSubmission(e) {
+    e.preventDefault();
+    this.model.addBook(genereateBookId(), this.getFormData());
+    // I'll need to clear the form and focus back to the title or close the modal
+  },
+  getFormData() {
+    const { title, author, pages } = this.bookForm;
+    return {
+      title: title.value,
+      author: author.value,
+      pages: pages.value,
+      completed: false,
+    };
+  },
+  handleUpdatingUI() {
+    this.view.updateBookshelfUI(this.model.getBookArray());
+    this.view.updateBookTotalUI(this.model.getTotalBooks());
+    this.view.updateCompletedBooksUI(this.model.getTotalOfCompletedBooks());
+    this.view.updateCompletionPercentageUI(this.model.getCompeletionScore());
+    this.view.updatePagesReadUI(this.model.getTotalPagesRead());
+  },
+  testingCompletedBooks() {
+    this.view.updateCompletedBooksUI(this.model.getTotalOfCompletedBooks());
+  },
+};
 
-// updateUI();
+Controller.prototype = Object.create(controllerMethod);
 
-// const template = document.getElementById("bookTemplate");
+const init = new Controller(new Library(), new ViewUI());
 
-// const clone = template.content.cloneNode(true);
-// clone.querySelector(".book__title").innerText = "Does it work";
-// clone.querySelector(".book__author").innerText = "Jazzy Martin";
-// clone.querySelector(".book").setAttribute("data-id", "1");
-// console.log(clone.querySelector(".book"));
-// document.querySelector(".bookshelf").appendChild(clone);
+// Utility functions
+function genereateBookId() {
+  return `${Date.now().toString(36) + Math.random().toString(36).substring(2)}`;
+}
+
+function updateInnerText(element, text) {
+  element.innerText = text;
+}
 
 // Modal controls
 const modal = document.querySelector("#modal");
--- test2.js
+++ test2.js
@@ -36,8 +36,89 @@ Healer.prototype.heal = function () {
 const hero1 = new Warrior("Bjorn", 1, "axe");
 const hero2 = new Healer("Kanin", 1, "cure");
 
-console.log(hero1.hasOwnProperty("name"));
-console.log(hero1.greet());
-console.log(hero2.greet());
-console.log(hero1.attack());
-console.log(hero2.heal());
+// console.log(hero1.hasOwnProperty("name"));
+// console.log(hero1.greet());
+// console.log(hero2.greet());
+// console.log(hero1.attack());
+// console.log(hero2.heal());
+
+function Library() {
+  this.library = new Map();
+  this.bookStats = {
+    totalBooks: 0,
+    booksFinished: 0,
+    totalPagesRead: 0,
+    completionScore: 0,
+  };
+}
+
+// needs methods for create, delete, update
+const libraryMethods = {
+  addBook(id, book) {
+    this.library.set(id, book);
+  },
+  deleteBook(id) {
+    this.library.delete(id);
+  },
+  updateBook(id, bool) {
+    this.library.get(id).completed = bool;
+    // this.library.set(id, {...this.library.get(id), completed: bool})
+  },
+};
+
+const bookMethods = {
+  getBookArray() {
+    return Array.from(this.library.values());
+  },
+  // get book stats
+  setTotalBooks() {
+    this.bookStats.totalBooks = this.library.size;
+  },
+  setCompletedBooks() {
+    const numOfCompleted = this.getBookArray().filter(
+      (book) => book.completed
+    ).length;
+    this.bookStats.booksFinished = numOfCompleted;
+  },
+  //calculate total pages read
+  getTotalPagesRead() {
+    return this.getBookArray().reduce(
+      (acc, curr) => (acc += curr.completed ? curr.pages : 0),
+      0
+    );
+  },
+  // calculate completion
+  setCompeletionScore() {
+    let { totalBooks, booksFinished } = bookStats;
+    this.bookStats.completionScore = (booksFinished / totalBooks) * 100;
+  },
+};
+
+Library.prototype = Object.create({ ...libraryMethods, ...bookMethods });
+
+const myLibrary = new Library();
+myLibrary.addBook(1, {
+  title: "Book",
+  author: "Kevin",
+  pages: 100,
+  completed: false,
+});
+myLibrary.addBook(2, {
+  title: "Book 2",
+  author: "Kim",
+  pages: 200,
+  completed: false,
+});
+// myLibrary.deleteBook(1)
+myLibrary.updateBook(2, true);
+myLibrary.setTotalBooks();
+myLibrary.setCompletedBooks();
+console.dir(myLibrary);
+// console.log(myLibrary.getTotalPagesRead())
+
+function UI() {
+  // needs the array from the Library function to work properly
+  // need a way to create a card
+  // send an array of cards to the DOM
+  // update stats on the DOM
+}
